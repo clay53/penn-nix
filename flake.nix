cfa4b037c40925b7fc0a49026fc525a548107088
clay53 <clayton@claytondoesthings.xyz>
2025-02-15 23:02:06 -0500

    fix all names being overridden

--- flake.nix
+++ flake.nix
@@ -10,11 +10,38 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
-  import ./waypoint-client.nix { inherit nixpkgs; x86_64-linux-bin = inputs.waypoint-client-bin-x86_64-linux; }
-  // import ./penn-clubs.nix { inherit nixpkgs; }
-  // import ./platform.nix { inherit nixpkgs; }
-  // import ./penn-courses.nix { inherit nixpkgs; }
-  // import ./ohq.nix { inherit nixpkgs; }
-  // import ./penn-mobile.nix { inherit nixpkgs; }
-  ;
+    let
+      mergeSystems = oldSystems: newSystems:
+        builtins.foldl'
+          (acc: systemName: {
+            ${systemName} =
+              if acc ? ${systemName}
+              then acc.${systemName} // newSystems.${systemName}
+              else newSystems.${systemName};
+          })
+          oldSystems
+          (builtins.attrNames newSystems);
+      mergeOutputs = outputs:
+        builtins.foldl'
+          (acc: output: {
+            packages =
+              if output ? packages
+              then mergeSystems acc.packages output.packages
+              else acc.packages;
+            devShells =
+              if output ? devShells
+              then mergeSystems acc.devShells output.devShells
+              else acc.devShells;
+          })
+          { packages = {}; devShells = {}; }
+          outputs;
+    in
+    mergeOutputs [
+      (import ./waypoint-client.nix { inherit nixpkgs; x86_64-linux-bin = inputs.waypoint-client-bin-x86_64-linux; })
+      (import ./penn-clubs.nix { inherit nixpkgs; })
+      (import ./platform.nix { inherit nixpkgs; })
+      (import ./penn-courses.nix { inherit nixpkgs; })
+      (import ./ohq.nix { inherit nixpkgs; })
+      (import ./penn-mobile.nix { inherit nixpkgs; })
+    ];
 }
